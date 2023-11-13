c7374b4b1b117a970ac03f73f16b1a75b6a72ef4
Guemiza <nesrine.f@live.com>
2023-11-13 22:06:55 +0100

    update code source

--- git.sh
+++ git.sh
@@ -4,4 +4,4 @@ password =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
 git add .
 git commit -m "update code source"
 git branch 
-GIT_ASKPASS="./git-askpass.sh" GIT_USERNAME="$Username" GIT_PASSWORD="$password" git push -u origin dockerfile
+GIT_ASKPASS= GIT_USERNAME="$Username" GIT_PASSWORD="$password" git push -u origin dockerfile
